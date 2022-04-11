f2308205187b8b66711e75bbe6cbec31926d4131
mrgomez84 <84170744+mrgomez84@users.noreply.github.com>
2022-04-10 21:05:47 -0600

    2do Commit
    Actualizaciond e Colores y Estilos

--- Calculadora/app/src/main/java/com/calculadora/app/MainActivity.kt
+++ Calculadora/app/src/main/java/com/calculadora/app/MainActivity.kt
@@ -8,6 +8,11 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+    private var num1: Double = 0.0
+    private var num2: Double = 0.0
+    private var operacion: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +29,17 @@ class MainActivity : AppCompatActivity() {
         val btn8 : Button = findViewById(R.id.btn8)
         val btn9 : Button = findViewById(R.id.btn9)
         val btn0 : Button = findViewById(R.id.btn0)
+
         val btnPunto : Button = findViewById(R.id.btnPunto)
+
+        val btnSuma : Button = findViewById(R.id.btnSuma)
+        val btnResta : Button = findViewById(R.id.btnResta)
+        val btnMultiplicasion : Button = findViewById(R.id.btnMultiplicacion)
+        val btnDivision : Button = findViewById(R.id.btnDivision)
+
+        val btnBorrar : Button = findViewById(R.id.btnBorrar)
+
+        val btnIgual : Button = findViewById(R.id.btnIgual)
 
         btn1.setOnClickListener{ numeroPresionado("1")}
         btn2.setOnClickListener{ numeroPresionado("2")}
@@ -37,11 +52,60 @@ class MainActivity : AppCompatActivity() {
         btn9.setOnClickListener{ numeroPresionado("9")}
         btn0.setOnClickListener{ numeroPresionado("0")}
         btnPunto.setOnClickListener{ numeroPresionado(".")}
+
+        btnBorrar.setOnClickListener{
+            val resultadoTextView : TextView = findViewById(R.id.resultadoTextView)
+            num1 = 0.0
+            num2 = 0.0
+            resultadoTextView.text = ""
+            operacion = NO_OPERACION
+        }
+
+        btnSuma.setOnClickListener { operacionPresionada(SUMA) }
+        btnResta.setOnClickListener { operacionPresionada(RESTA) }
+        btnMultiplicasion.setOnClickListener { operacionPresionada(MULTIPLICACION) }
+        btnDivision.setOnClickListener { operacionPresionada(DIVISION) }
+
+        btnIgual.setOnClickListener {
+            var resultado = when(operacion){
+                SUMA -> num1 + num2
+                RESTA -> num1 - num2
+                MULTIPLICACION -> num1 * num2
+                DIVISION -> num1 / num2
+                else -> 0
+            }
+            val resultadoTextView : TextView = findViewById(R.id.resultadoTextView)
+            resultadoTextView.text = resultado.toString()
+        }
     }
 
     private fun numeroPresionado(digito: String){
         val resultadoTextView : TextView = findViewById(R.id.resultadoTextView)
+
         resultadoTextView.text = "${resultadoTextView.text}$digito"
+
+        if (operacion == NO_OPERACION){
+            num1 = resultadoTextView.text.toString().toDouble()
+        }else{
+            num2 = resultadoTextView.text.toString().toDouble()
+        }
+    }
+
+    private fun operacionPresionada(operacion: Int){
+        this.operacion = operacion
+
+        val resultadoTextView : TextView = findViewById(R.id.resultadoTextView)
+        /*num1 = resultadoTextView.text.toString().toDouble()*/
+
+        resultadoTextView.text = ""
+    }
+
+    companion object{
+        const val SUMA = 1
+        const val RESTA = 2
+        const val MULTIPLICACION = 3
+        const val DIVISION = 4
+        const val NO_OPERACION = 0
     }
 
     private fun darkOff(){        ///////////OF MODE DARK/////////
@@ -52,7 +116,6 @@ class MainActivity : AppCompatActivity() {
                 /* si esta activo el modo oscuro lo desactiva */AppCompatDelegate.setDefaultNightMode(
                     AppCompatDelegate.MODE_NIGHT_NO
                 )
-
             }
         }
     }
